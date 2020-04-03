08177b8d167ce4f1d82a94eb4819656e32886cba
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-04-03 14:37:28 +0800

    6_2

--- 6_2/6_2.cpp
+++ 6_2/6_2.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv.hpp>
+using namespace cv;
 
 int main()
 {
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\lena.jpg", 1);
+	if (srcMat.empty()) return -1;
+
+	float angel = -10.0, scale = 1;
+	cv::Point2f center(srcMat.cols*0.5, srcMat.rows*0.5);
+	const cv::Mat affine_matrix = cv::getRotationMatrix2D(center, angel, scale);
+	
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+
+	cv::imshow("src", srcMat);
+	cv::imshow("dat", dstMat);
+	cv::waitKey(0);
     return 0;
 }
 
